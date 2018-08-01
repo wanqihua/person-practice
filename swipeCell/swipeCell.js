a33325b653f61e33a28b5ab4f20edf4976bb67e5
wanqihua <wanqhnice@163.com>
2018-08-02 01:27:45 +0800

    801 防抖节流

--- swipeCell/swipeCell.js
+++ swipeCell/swipeCell.js
@@ -122,3 +122,55 @@ function pullRefresh(){
     }
   };
 }
+
+// 防抖动函数
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+var myEfficientFn = debounce(function() {
+  console.log(this);
+}, 250);
+
+// 绑定监听
+window.addEventListener('scroll', myEfficientFn);
+
+
+// 简单的节流函数
+function throttle(func, wait, mustRun) {
+  var timeout,
+    startTime = new Date();
+
+  return function() {
+    var context = this,
+      args = arguments,
+      curTime = new Date();
+
+    clearTimeout(timeout);
+    // 如果达到了规定的触发时间间隔，触发 handler
+    if(curTime - startTime >= mustRun){
+      func.apply(context,args);
+      startTime = curTime;
+      // 没达到触发间隔，重新设定定时器
+    }else{
+      timeout = setTimeout(func, wait);
+    }
+  };
+};
+// 实际想绑定在 scroll 事件上的 handler
+function realFunc(){
+  console.log("Success");
+}
+// 采用了节流函数
+window.addEventListener('scroll',throttle(realFunc,500,1000));
